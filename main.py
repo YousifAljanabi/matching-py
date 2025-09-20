532b5735e6e4fce748e30054e92f95704747e362
YousifAljanabi <yosif.ph2003@gmail.com>
2025-09-20 20:57:10 +0300

    CORS allow

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.modules.users.router import router as users_router
 from app.modules.room.router import router as rooms_router
 from app.modules.access.router import router as access_router
 from app.modules.log.router import router as logs_router
 
 app = FastAPI(title="Home Security API", version="1.0.0")
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Include all routers
 app.include_router(users_router)
