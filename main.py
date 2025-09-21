5c2656d0130d772b16a442599fd6c5961ea69ec5
YousifAljanabi <yosif.ph2003@gmail.com>
2025-09-21 17:56:15 +0300

    removed the pipeline

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from app.modules.users.router import router as users_router
 from app.modules.room.router import router as rooms_router
 from app.modules.access.router import router as access_router
 from app.modules.log.router import router as logs_router
-from app.modules.normalize_phone.pipeline import router as pipeline_router
+# from app.modules.normalize_phone.pipeline import router as pipeline_router
 
 app = FastAPI(title="Home Security API", version="1.0.0")
 
@@ -22,7 +22,7 @@ app.include_router(users_router)
 app.include_router(rooms_router)
 app.include_router(access_router)
 app.include_router(logs_router)
-app.include_router(pipeline_router)
+# app.include_router(pipeline_router)
 
 
 @app.get("/")
